c1b164d8365d10e8d7d831efd4971bd7783d6378
Kristy Nguyen <kln95624@gmail.com>
2023-03-16 16:33:50 -0700

    Editted AboutHeadSection component, Added AboutHeadSectionOptions and AboutHeadSectionPreview image, Added component to registeredComponents map in utils

--- src/components/AboutHeadSection/AboutHeadSection.jsx
+++ src/components/AboutHeadSection/AboutHeadSection.jsx
@@ -1,28 +1,20 @@
-import React from 'react';
-import './styles.css';
-
-function AboutPage() {
-  const name = 'Kartik Bansal';
-  const description = `Over the past 12 years, I\'ve worked with a diverse 
-                      range of clients, from startups to Fortune 500 companies.
-                      I love crafting interfaces that delight users and help 
-                      businesses grow.`;
+export default function AboutPage({options}) {
+  const {name, description} = options;
 
   return (
     <div className="flex flex-wrap">
       <div className="w-full md:w-1/2 p-4">
-        <h1 className="text-6xl font-bold mb-4">
+        <h1 className="text-6xl font-bold mb-4 bg-white text-black dark:bg-black dark:text-white">
         Hi, I am
         <br />
         {name}
         </h1>
       </div>
 
-      <div className="w-full md:w-1/2 p-4">
+      <div className="w-full md:w-1/2 p-4 bg-white text-black dark:bg-black dark:text-white">
         <p>{description}</p>
       </div>
     </div>
+
   );
 }
-
-export default AboutPage;
---
+++ src/components/AboutHeadSection/AboutHeadSectionOptions.jsx
@@ -0,0 +1,20 @@
+import { Gradients } from "../../utils/utils";
+
+export default function AboutHeadSectionOptions({options, updateComponent}) {
+    let {name, description} = options;
+
+    function handleChange(e) {
+        updateComponent({ [e.target.name]: e.target.value});
+    }
+
+    
+    return (
+        <div className="p-3">
+            <label className="block">Name:</label>
+            <input className={`block mb-3 p-2 rounded-lg bg-gray-200`} type="text" name="name" value={name} onChange={(handleChange)} />
+
+            <label className="block">Description:</label>
+            <input className={`block mb-3 p-2 rounded-lg bg-gray-200`} type="text" name="description" value={description} onChange={(handleChange)} />
+        </div>
+    )
+}

b637a3705995d51c25cda7ac0049f055249267ca
Yunhuiww <122959510+Thindatuo@users.noreply.github.com>
2023-03-16 21:54:41 -0700

    404 section modified and follow me container added (half finished)

--- src/components/404Section/FourZeroFour.jsx
+++ src/components/404Section/FourZeroFour.jsx
@@ -3,52 +3,46 @@ export default function FourZeroFourDark() {
         <div classname = "four-zero-four-dark">    
             <body bgcolor = "#222222">    
                 <img scr = "https://www.figma.com/file/5hkfaxoU9R1e1hikIQQ2Sb/Portfolio-website-UI-Kit?node-id=128-1928&t=mnLfkxiCK10hTXjR-4"
-                    width = 340 height = 221.21>
+                 width = "340" height = "221.21" />
             
             <style> padding: 64px; </style>
                 
-            <p style = {
-                position: absolute;
-                width: 138px;
-                height: 90px;
-                left: calc(50% - 138px/2 - 101px);
-                top: 275.21px;
-
-                /* Heading 1 */
-                font-family: 'Bai Jamjuree';
-                font-style: normal;
-                font-weight: 700;
-                font-size: 72px;
-                line-height: 125%;
-                /* identical to box height, or 90px */
-                display: flex;
-                align-items: center;
-
-                color: #FFFFFF;
-            }>404</p>    
+            <p style = {{
+                position: 'absolute',
+                width: '138px',
+                height: '90px',
+                left: 'calc(50% - 138px/2 - 101px)',
+                top: '275.21px',
+                fontFamily: 'Bai Jamjuree',
+                fontStyle: 'normal',
+                fontWeight: 700,
+                fontSize: '72px',
+                lineHeight: '125%',
+                display: 'flex',
+                alignItems: 'center',
+                color: '#FFFFFF',
+            }}>404</p>    
 
             <p></p>
             <p></p>
-            /* Page not found */
-            <p style = {
-                position: absolute;
-                width: 176px;
-                height: 30px;
-                left: calc(50% - 176px/2 - 82px);
-                top: 389.21px;
-
-                /* Heading 6 */
-                font-family: 'Bai Jamjuree';
-                font-style: normal;
-                font-weight: 700;
-                font-size: 24px;
-                line-height: 125%;
+            
+            <p style = {{
+                position: 'absolute',
+                width: '176 px',
+                height: '30 px',
+                left: 'calc(50% - 176 px/2 - 82 px)',
+                top: '389.21 px',
+                //font-family: 'Bai Jamjuree',
+                //font-style: 'normal',
+                fontWeight: '700',
+                fontSize: '24 px',
+                //lineHeight: 125%,
                 /* identical to box height, or 30px */
-                display: flex;
-                align-items: center;
+                display: 'flex',
+                alignItems: 'center',
 
-                color: #FFFFFF;
-            }>Page Not Found</p>
+                color: '#FFFFFF',
+            }}>Page Not Found</p>
 
             <p></p>
             <button className = "flex h-14 bg-gray-200 items-center"> </button>
@@ -58,62 +52,3 @@ export default function FourZeroFourDark() {
     )
 }
 
-export default function FourZeroFourLight() {
-    return (
-        <div classname = "four-zero-four-dark">    
-            <body bgcolor = "#E7E8E5">    
-                <img scr = "https://www.figma.com/file/5hkfaxoU9R1e1hikIQQ2Sb/Portfolio-website-UI-Kit?node-id=128-1928&t=sJLUtMwWE87rriwH-4"
-                    width = 340 height = 221.21>
-            
-            <style> padding: 64px; </style>
-                
-            <p style = {
-                position: absolute;
-                width: 138px;
-                height: 90px;
-                left: calc(50% - 138px/2 - 101px);
-                top: 275.21px;
-
-                /* Heading 1 */
-                font-family: 'Bai Jamjuree';
-                font-style: normal;
-                font-weight: 700;
-                font-size: 72px;
-                line-height: 125%;
-                /* identical to box height, or 90px */
-                display: flex;
-                align-items: center;
-
-                color: #222222;
-            }>404</p>    
-
-            <p></p>
-            <p></p>
-            /* Page not found */
-            <p style = {
-                position: absolute;
-                width: 176px;
-                height: 30px;
-                left: calc(50% - 176px/2 - 82px);
-                top: 389.21px;
-
-                /* Heading 6 */
-                font-family: 'Bai Jamjuree';
-                font-style: normal;
-                font-weight: 700;
-                font-size: 24px;
-                line-height: 125%;
-                /* identical to box height, or 30px */
-                display: flex;
-                align-items: center;
-
-                color: #222222;
-            }>Page Not Found</p>
-
-            <p></p>
-            <button className = "flex h-14 bg-gray-200 items-center"> </button>
-
-            </body>
-        </div>
-    )
-}

a087ae7c145cbc5f23b5ffd305f1c7c0b79d7b73
Kristy Nguyen <kln95624@gmail.com>
2023-03-17 17:32:55 -0700

    Formatting was added to the About Head Section

--- src/components/AboutHeadSection/AboutHeadSection.jsx
+++ src/components/AboutHeadSection/AboutHeadSection.jsx
@@ -1,20 +1,23 @@
 export default function AboutHeadSection({options}) {
-  const {name, description} = options;
+  const {name, gradient, description} = options;
 
   return (
-    <div className="flex flex-wrap">
-      <div className="w-full md:w-1/2 p-4">
-        <h1 className="text-6xl font-bold mb-4 bg-white text-black dark:bg-black dark:text-white">
+    <div className="p-4 flex flex-wrap">
+      <div className="pt-56 pl-32 pr-134 w-full md:w-1/2 p-4">
+        <h1 className="text-4xl font-bold font-main bg-white text-black dark:bg-black dark:text-white">
         Hi, I am
         <br />
         {name}
+        {/*<span className="bg-clip-text text-transparent bg-gradient-to-r ${gradient}">{name}</span>*/}
         </h1>
       </div>
 
-      <div className="w-full md:w-1/2 p-4 bg-white text-black dark:bg-black dark:text-white">
+      <div className="pt-56 pr-32 pl-134 max-w-[605px] bg-white text-black dark:bg-black dark:text-white">
         <p>{description}</p>
       </div>
     </div>
+
+
 
   );
 }
--- src/components/AboutHeadSection/AboutHeadSectionOptions.jsx
+++ src/components/AboutHeadSection/AboutHeadSectionOptions.jsx
@@ -1,5 +1,7 @@
+import GradientSelect from "../GradientSelect/GradientSelect";
+
 export default function AboutHeadSectionOptions({options, updateComponent}) {
-    let {name, description} = options;
+    let {name, gradient, description} = options;
 
     function handleChange(e) {
         updateComponent({ [e.target.name]: e.target.value});
@@ -10,6 +12,10 @@ export default function AboutHeadSectionOptions({options, updateComponent}) {
         <div className="p-3">
             <label className="block">Name:</label>
             <input className={`block mb-3 p-2 rounded-lg bg-gray-200`} type="text" name="name" value={name} onChange={(handleChange)} />
+
+            
+            <GradientSelect name={"gradient"} value={gradient} handleChange={handleChange} />
+            
 
             <label className="block">Description:</label>
             <input className={`block mb-3 p-2 rounded-lg bg-gray-200`} type="text" name="description" value={description} onChange={(handleChange)} />

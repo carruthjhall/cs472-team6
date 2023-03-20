cdf69546b5ef2e5f8a7487323d148ddc9299c5dd
Zach C <zacheryec001@gmail.com>
2023-03-20 16:06:14 -0700

    Added Testimonial component

--- src/utils/registeredComponents.js
+++ src/utils/registeredComponents.js
@@ -1,9 +1,14 @@
 import Banner from "../components/Banner/Banner";
 import BannerOptions from "../components/Banner/BannerOptions";
 import BannerPreview from "../components/Banner/BannerPreview.png";
+import Testimonial from "../components/Testimonial/Testimonial";
+import TestimonialOptions from "../components/Testimonial/TestimonialOptions";
+import TestimonialPreview from "../components/Testimonial/TestimonialPreviewLight.png";
+import defaultPFP from "../components/Testimonial/Default_pfp.svg";
 import Component from "../models/Component";
 import { Gradients } from "./utils";
 
 export const registeredComponents = new Map([
-    ['Banner', new Component('Banner', Banner, BannerOptions, BannerPreview, {words: 'Design,Develop,Discover', gradient: Gradients.default})]
+    ['Banner', new Component('Banner', Banner, BannerOptions, BannerPreview, {words: 'Design,Develop,Discover', gradient: Gradients.default})],
+    ['Testimonial', new Component('Testimonial', Testimonial, TestimonialOptions, TestimonialPreview, {stars: 5, starGradient: Gradients.default, testimonialBody: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor sit amet consectetur adipiscing elit pellentesque habitant.", custPicture: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/240px-Default_pfp.svg.png", custName: "Insert customer name here", custPosition: "Insert customer position here"})]
 ]);

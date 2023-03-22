249e729853a71876041226c6f01667619f454c0b
Kristy Nguyen <kln95624@gmail.com>
2023-03-21 17:31:39 -0700

    Deleted files from AboutHeadImages component to separate component issues

--- src/utils/registeredComponents.js
+++ src/utils/registeredComponents.js
@@ -6,14 +6,6 @@ import AboutHeadSection from "../components/AboutHeadSection/AboutHeadSection";
 import AboutHeadSectionOptions from "../components/AboutHeadSection/AboutHeadSectionOptions";
 import AboutHeadSectionPreview from "../components/AboutHeadSection/AboutHeadSectionPreview.png";
 
-import AboutHeadImages from "../components/AboutHeadImages/AboutHeadImages";
-import AboutHeadImagesOptions from "../components/AboutHeadImages/AboutHeadImagesOptions";
-import AboutHeadImagesPreview from "../components/AboutHeadImages/AboutHeadImagesPreview.png";
-import image1 from "../components/AboutHeadImages/aboutImage1.svg";
-import image2 from "../components/AboutHeadImages/aboutImage2.svg";
-import image3 from "../components/AboutHeadImages/aboutImage3.svg";
-import image4 from "../components/AboutHeadImages/aboutImage4.svg";
-
 import Component from "../models/Component";
 import { Gradients } from "./utils";
 
@@ -24,13 +16,6 @@ export const registeredComponents = new Map([
             gradient: Gradients.default,
             description: `Over the past 12 years, I\'ve worked with a diverse range of clients, from startups to Fortune 500 companies. 
                             I love crafting interfaces that delight users and help businesses grow.`
-        })
-    ],
-    ['AboutHeadImages', new Component('AboutHeadImages', AboutHeadImages, AboutHeadImagesOptions, AboutHeadImagesPreview, 
-        {   image1: {image1}, 
-            image2: {image2},
-            image3: {image3},
-            image4: {image4}
         })
     ],
 ]);

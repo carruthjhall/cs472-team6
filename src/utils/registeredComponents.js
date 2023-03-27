0eec1bec01a32d46af407fefe4ad62ffbffe9d41
ebacolod <ebacolod25@gmail.com>
2023-03-27 14:00:15 -0700

    Simplified EducationAndWork registered components

--- src/utils/registeredComponents.js
+++ src/utils/registeredComponents.js
@@ -5,9 +5,7 @@ import {BannerComponent} from '../components/Banner/BannerComponent';
 import StatsWrapper from "../components/StatsWrapper/StatsWrapper";
 import StatsWrapperOptions from "../components/StatsWrapper/StatsWrapperOptions";
 import StatsWrapperPreview from "../components/StatsWrapper/StatsWrapperPreview.png";
-import EducationWorkExperience from "../components/EducationAndWork/EducationAndWork";
-import EducationWorkExperienceOptions from "../components/EducationAndWork/EducationAndWorkOptions";
-import EducationWorkExperiencePreview from "../components/EducationAndWork/EducationAndWorkDark.png";
+import {EducationAndWorkComponent} from '../components/EducationAndWork/EducationAndWorkComponent';
 import Skills from "../components/Skills/Skills";
 import SkillsOptions from "../components/Skills/SkillsOptions";
 import SkillsPreview from "../components/Skills/SkillsDark.png";
@@ -22,24 +20,7 @@ export const registeredComponents = new Map([
     ['Banner', BannerComponent],
     ['Stats Section', new Component('Stats Section', StatsWrapper, StatsWrapperOptions, StatsWrapperPreview, {stats: [{number: "12+", text: "Years of Experience in Design"},{number: "100+", text: "Successfully Completed Projects"},{number: "50+", text: "Global Customers"}], gradient: Gradients.default})],
     ['CtaSection', new Component('CtaSection', CtaSection, CtaSectionOptions, CtaSectionPreview, {link: "https://www.google.com/", word1: "See the impact of good, conversion-oriented design on your business.", word2: "Let's Work Together", gradient: Gradients.default})],
-    [
-        'Education and Work',
-        new Component(
-          'Education and Work',
-          EducationWorkExperience,
-          EducationWorkExperienceOptions,
-          EducationWorkExperiencePreview,
-          {
-            school1: 'School 1; Degree; Date',
-            school2: 'School 2; Degree; Date',
-            school3: 'School 3; Degree; Date',
-            job1: 'Company 1; Title; Date',
-            job2: 'Company 2; Title; Date',
-            job3: 'Company 3; Title; Date',
-            gradient: Gradients.default
-          }
-        )
-    ],
+    ['Education and Work', EducationAndWorkComponent],
     [
       'Skills',
       new Component(

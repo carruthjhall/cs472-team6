048d111a08d50536ef69031190b4afd74d43310d
WuliuQ <doubixian@gmail.com>
2023-03-28 15:39:59 -0700

    Update BlogPage.jsx

--- src/components/BlogPage/BlogPage.jsx
+++ src/components/BlogPage/BlogPage.jsx
@@ -3,7 +3,7 @@ export default function BlogPage({options}) {
     const {overview, paragraph1, image,} = options;
     const valueArray = paragraph1.split(String.fromCharCode(10));
     return (
-        <div className={`bg-white text-black dark:bg-black dark:text-white py-6 px-40`}>
+        <div className={`bg-white text-black dark:bg-black dark:text-white py-6 lg:px-40 md:px-20 px-10`}>
         <div className= "font-normal font-baijamjuree">
             <h2 className='mb-4 font-bold text-3xl leading-10'>{overview}</h2> 
             {image && (<img src={image} class="w-full h-auto aspect-w-1 aspect-h-1" alt="blogpicture"/>)}

3cd766f6439895fc296f2a22957b5df2f9cf81bb
WuliuQ <doubixian@gmail.com>
2023-03-28 19:40:52 -0700

    Update BlogPage.jsx

--- src/components/BlogPage/BlogPage.jsx
+++ src/components/BlogPage/BlogPage.jsx
@@ -7,8 +7,8 @@ export default function BlogPage({options}) {
         <div className= "font-normal font-baijamjuree">
             <h2 className='mb-4 font-bold text-3xl leading-10'>{overview}</h2> 
             {image && (<img src={image} class="w-full h-auto aspect-w-1 aspect-h-1" alt="blogpicture"/>)}
-            <div className="font-inter font-normal  
-            sm:text-base sm:leading-relaxed 
+            <div className="font-inter font-normal font-main
+            sm:text-sm sm:leading-relaxed 
             md:text-lg md:leading-6
             lg:text-lg lg:leading-6">{valueArray.map((value, index) => (
             <p key={index} className="my-paragraph mt-8">{value}</p>

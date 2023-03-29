aee1e3d4f850f276f1d140efeb4c4245ca8c7920
Zach C <zacheryec001@gmail.com>
2023-03-28 20:40:16 -0700

    Fixed img url bug

--- src/components/Companies/CompaniesOptions.jsx
+++ src/components/Companies/CompaniesOptions.jsx
@@ -7,6 +7,13 @@ export default function CompaniesOptions({options, updateComponent}){
         updateComponent({[event.target.name]: event.target.value});
     }
 
+    function handleLogoChange(index, event){
+        let newLogos = [...logoList];
+        newLogos[index] = {...newLogos[index], [event.target.name]: event.target.value};
+
+        updateComponent({logoList: newLogos});
+    }
+
     return(
         <div>
         <label className="block">Number of Companies</label>
@@ -14,19 +21,19 @@ export default function CompaniesOptions({options, updateComponent}){
         <GradientSelect name={"gradient"} value={gradient} handleChange={handleChange}/>
         {logoList.slice(0,noOfLogos).map((company, index) => {
                     return(
-                        <CompanyOptions key = {index} index = {index} company={company} handleChange={handleChange}/>
+                        <CompanyOptions key={index} index={index} company={company} handleLogoChange={handleLogoChange}/>
                     )
                 })}
         </div>
     );
 }
 
-function CompanyOptions({company, index, handleChange}){
+function CompanyOptions({company, index, handleLogoChange}){
     const {url, darkUrl} = company;
 return(
     <div>
         <label className="block">Logo {index + 1} URL</label>
-        <input className="block mb-3 p-2 rounded-lg bg-gray-200" type="url" name={`logo ${index + 1}`} value={url} onChange={(handleChange)}></input>
+        <input className="block mb-3 p-2 rounded-lg bg-gray-200" type="url" name={`url`} value={url} onChange={event => handleLogoChange(index, event)}></input>
     </div>
 );
 }

2155c5353d99a0dee5b1e3db1c0770e842447349
Yunhuiww <122959510+Thindatuo@users.noreply.github.com>
2023-04-04 16:06:03 -0700

    Update FourZeroFour.jsx
    add responsiveness to screen size of a phone to 404 component

--- src/components/404Section/FourZeroFour.jsx
+++ src/components/404Section/FourZeroFour.jsx
@@ -3,26 +3,19 @@ import fourZeroFourImage from "./404_img.png";
 export default function FourZeroFour({ options }) {
     const {buttonText, buttonLink, gradient} = options;
     return (
-        <div className="lg:w-[984px] md:h-[351.6px]
-                        md:w-[831px] md:h-[320px]
-                        sm:w-[340px] md:h-[513.21px]
-                        md:flex mx-auto md:content-center md:items-center">
-            <div className="flex-1"></div>
-            <div className="flex-none w-5/6">
-                <div className="flex w-full">
-                    <div className="flex-auto">
-                        <img className="lg:w-[566] lg:h-[351.6] md:w-[480] md:h-[298] sm:w-[340] sm:h-[211]" src={fourZeroFourImage}></img>
-                    </div>
-                    <div className="flex-1 my-auto mx-auto items-center content-center justify-center">
-                        <p className="font-main text-8xl dark:text-white text-black">404</p>
-                        <p className="font-main text-2xl sm:text-1x1 py-4 leading-tight dark:text-white text-black">Page not found</p>
-                        <button className={`w-[200px] h-[60px] rounded-[50px] mt-5 bg-gradient-to-r ${gradient}`}>
-                            <a href={buttonLink} className = "font-main dark:text-white text-black">BACK TO HOMEPAGE</a>
-                        </button>
-                    </div>
+        <div className="flex items-center justify-center">  
+            <div className="flex sm:flex-col md:flex-row md:gap-35 sm:gap-20 max-auto">
+                <div className="flex-initial ">
+                    <img className="lg:w-[566] lg:h-[351.6] md:w-[480] md:h-[298] sm:w-[340] sm:h-[211]" src={fourZeroFourImage}></img>
+                </div>
+                <div className="flex-initial">
+                    <p className="font-main flex-auto sm:text-6xl md:text-8xl dark:text-white text-black">404</p>
+                    <p className="font-main md:text-3xl sm:text-2xl py-4 leading-tight dark:text-white text-black">Page not found</p>
+                    <button className={`w-[250px] h-[60px] rounded-[50px] mt-5 bg-gradient-to-r ${gradient}`}>
+                        <a href={buttonLink} className = "font-main dark:text-white text-black">BACK TO HOMEPAGE</a>
+                    </button>
                 </div>
             </div>
-            <div className="flex-1"></div>
         </div>
     )
 }

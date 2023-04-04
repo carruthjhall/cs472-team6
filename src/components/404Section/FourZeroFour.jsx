7d1480cba902d9b84f0161287598af2bad6e7938
Yunhuiww <122959510+Thindatuo@users.noreply.github.com>
2023-04-04 16:18:24 -0700

    Update FourZeroFour.jsx
    image size and button size adjusted

--- src/components/404Section/FourZeroFour.jsx
+++ src/components/404Section/FourZeroFour.jsx
@@ -6,12 +6,12 @@ export default function FourZeroFour({ options }) {
         <div className="flex items-center justify-center">  
             <div className="flex sm:flex-col md:flex-row md:gap-35 sm:gap-20 max-auto">
                 <div className="flex-initial ">
-                    <img className="lg:w-[566] lg:h-[351.6] md:w-[480] md:h-[298] sm:w-[340] sm:h-[211]" src={fourZeroFourImage}></img>
+                    <img className="lg:w-full md:w-[480] md:h-[298] sm:w-[340] sm:h-[211]" src={fourZeroFourImage}></img>
                 </div>
                 <div className="flex-initial">
                     <p className="font-main flex-auto sm:text-6xl md:text-8xl dark:text-white text-black">404</p>
                     <p className="font-main md:text-3xl sm:text-2xl py-4 leading-tight dark:text-white text-black">Page not found</p>
-                    <button className={`w-[250px] h-[60px] rounded-[50px] mt-5 bg-gradient-to-r ${gradient}`}>
+                    <button className={`w-[200px] h-[50px] rounded-[50px] mt-5 bg-gradient-to-r ${gradient}`}>
                         <a href={buttonLink} className = "font-main dark:text-white text-black">BACK TO HOMEPAGE</a>
                     </button>
                 </div>

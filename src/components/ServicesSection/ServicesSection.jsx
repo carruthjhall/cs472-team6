6b03b7efb6c2b0f0013e731f2056f29365c52e61
Anthony Romaine <anthony.romaine1@gmail.com>
2023-04-12 21:25:56 -0700

    Finish styling for ServiceSection component

--- src/components/ServicesSection/ServicesSection.jsx
+++ src/components/ServicesSection/ServicesSection.jsx
@@ -1,8 +1,11 @@
+
+
 export default function ServicesSection({ options }){
     const { services } = options;
     return (
-        <section>
-            <h2>Services</h2>
+        <section className="p-10 max-w-[1200px] mx-auto my-[130px]">
+            <h2 className="font-main text-[64px] leading-tight font-bold dark:text-white">Services</h2>
+            {services.map((service, index) => <Service key={index} service={service} />)}
         </section>
     )
 }
@@ -10,6 +13,12 @@ export default function ServicesSection({ options }){
 function Service({service}) {
     const { name, gradient, picture } = service;
     return (
-        <div></div>
+        <div className={`group relative border-y-2 border-y-black border-collapse -mb-0.5 py-5 px-3 dark:border-y-white hover:bg-gradient-to-r ${gradient} cursor-default`}>
+            <p className="font-main font-bold text-[56px] leading-tight dark:text-white">{name}</p>
+            <img src={picture} className="h-[300px] w-[240px] absolute object-cover rounded-3xl hidden z-10 top-1/2 -translate-y-1/2 right-1/4 lg:group-hover:block"/>
+            <svg className="absolute hidden md:block top-1/2 -translate-y-1/2 right-3" width="56" height="57" viewBox="0 0 56 57" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path className="dark:stroke-white" d="M2 54.3433L54 2.34326M54 2.34326V50.3814M54 2.34326H5.46667" stroke="#161513" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+        </div>
     )
 }
--- src/components/ServicesSection/ServicesSectionComponent.js
+++ src/components/ServicesSection/ServicesSectionComponent.js
@@ -19,10 +19,10 @@ export const ServicesSectionComponent = new Component(
     ServicesSectionPreview, 
     { 
         services: [
-            new ServiceOptions('UI/UX Design', Gradients.default, null),
-            new ServiceOptions('Graphic Design', Gradients.default, null),
-            new ServiceOptions('Web Design', Gradients.default, null),
-            new ServiceOptions('Product Design', Gradients.default, null)
+            new ServiceOptions('UI/UX Design', Gradients.default, "https://images.unsplash.com/photo-1576153192396-180ecef2a715?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"),
+            new ServiceOptions('Graphic Design', Gradients.default, "https://images.unsplash.com/photo-1626785774573-4b799315345d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"),
+            new ServiceOptions('Web Design', Gradients.default, "https://images.unsplash.com/photo-1621839673705-6617adf9e890?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"),
+            new ServiceOptions('Product Design', Gradients.default, "https://images.unsplash.com/photo-1600697395543-ef3ee6e9af7b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80")
         ]
     }
 );

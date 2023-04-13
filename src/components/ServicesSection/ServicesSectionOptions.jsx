821cae9ce3e7fc30d03775d1c2ca27c5a41e4370
Anthony Romaine <anthony.romaine1@gmail.com>
2023-04-13 15:17:31 -0700

    Add options to add and delete services

--- src/components/ServicesSection/ServicesSectionOptions.jsx
+++ src/components/ServicesSection/ServicesSectionOptions.jsx
@@ -1,5 +1,7 @@
 import { useRef } from "react";
 import GradientSelect from "../GradientSelect/GradientSelect";
+import { ServiceOptions } from "./ServicesSectionComponent";
+import { Gradients } from "../../utils/utils";
 
 export default function ServicesSectionOptions({options, updateComponent}) {
     let {header, services} = options;
@@ -15,18 +17,32 @@ export default function ServicesSectionOptions({options, updateComponent}) {
         updateComponent({ services: newServices });
     }
 
+    function handleAddService(){
+        let newServices = [...services];
+        newServices.push(new ServiceOptions('UI/UX Design', Gradients.default, "https://images.unsplash.com/photo-1576153192396-180ecef2a715?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80", ''))
+        
+        updateComponent({ services: newServices })
+    }
+
+    function handleDeleteService(index){
+        let newServices = [...services];
+        newServices.splice(index, 1);
+        
+        updateComponent({ services: newServices })
+    }
+
     
     return (
         <div className="p-3">
             <label className="options-label">Header:</label>
             <input type="text" name="header" className="options-input" value={header} onChange={handleChange} />
-            <button className="options-btn bg-blue-500 hover:bg-blue-700 font-bold text-white">Add Item</button>
-            {services.map((service, index) => <ServiceOptions key={index} service={service} index={index} handleChange={handleServiceChange}  />)}
+            <button onClick={handleAddService} className="options-btn bg-blue-500 hover:bg-blue-700 font-bold text-white">Add Item</button>
+            {services.map((service, index) => <ServiceOption key={index} service={service} index={index} handleChange={handleServiceChange} handleDelete={handleDeleteService}  />)}
         </div>
     )
 }
 
-function ServiceOptions({index, service, handleChange}){
+function ServiceOption({index, service, handleChange, handleDelete}){
     const { name, gradient, picture, url } = service;
     const imageInput = useRef(null);
 
@@ -72,6 +88,7 @@ function ServiceOptions({index, service, handleChange}){
             <input ref={imageInput} name="picture" onChange={handleImageImport} className="hidden" type="file" accept="image/jpeg, image/png, image/jpg"></input>
             <button className="options-btn" onClick={handleSelect}>Select Image</button>
             <GradientSelect name={"gradient"} value={gradient} handleChange={e => handleChange(index, e)} />
+            <button onClick={() => handleDelete(index)} className="options-btn bg-red-500 hover:bg-red-700 font-bold text-white">Delete Item {index + 1}</button>
         </div>
     )
 }

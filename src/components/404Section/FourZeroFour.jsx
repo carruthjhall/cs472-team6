dac356ad68f9e734c8267896ad2ea4f1e861c361
Yunhuiww <122959510+Thindatuo@users.noreply.github.com>
2023-04-13 16:12:23 -0700

    Update FourZeroFour.jsx
    deleted sm prefix on line 7

--- src/components/404Section/FourZeroFour.jsx
+++ src/components/404Section/FourZeroFour.jsx
@@ -4,7 +4,7 @@ export default function FourZeroFour({ options }) {
     const {buttonText, buttonLink, gradient} = options;
     return (
         <div className="flex items-center justify-center">  
-            <div className="flex flex-col md:flex-row md:gap-35 sm:gap-20 max-auto">
+            <div className="flex flex-col md:flex-row md:gap-35 gap-20 max-auto">
                 <div className="flex-initial ">
                     <img className="lg:w-full md:w-[480] md:h-[298] w-[340] h-[211]" src={fourZeroFourImage}></img>
                 </div>

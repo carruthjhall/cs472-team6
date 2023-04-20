447f8f5497fc150102e38826fab567f7c3a5579e
WuliuQ <doubixian@gmail.com>
2023-04-20 10:37:14 -0700

    Update CtaSection2.jsx

--- src/components/CtaSection2/CtaSection2.jsx
+++ src/components/CtaSection2/CtaSection2.jsx
@@ -3,7 +3,7 @@ export default function CtaSection2({options}) {
   const {link, text, button, gradient} = options;
   
   return (
-    <div className={`bg-white text-black dark:bg-black dark:text-white py-10 lg:px-[120px] md:px-[80px] px-[25px]`}>
+    <div className={`bg-white text-black dark:bg-black dark:text-white py-10 lg:px-[120px] md:px-[80px] px-[25px] lg:max-w-[1200px] md:max-w-[864px] max-w-[340px] mx-auto`}>
     <div className ={`bg-gradient-to-r ${gradient} 
     lg:rounded-bl-[128px] lg:rounded-br-[128px] 
     md:rounded-bl-[92px] md:rounded-br-[92px] 
@@ -12,13 +12,13 @@ export default function CtaSection2({options}) {
     flex justify-center items-center`}>
     <div class="flex flex-col items-center text-center">
     <h2 class="font-bold font-baijamjuree 
-    lg:text-[84px] lg:px-60 lg:mt-[100px]
-    md:text-[72px] md:px-atuo md:mt-[47px]
-    text-[32px] px-20 mt-[28px]">{text}</h2>
+    lg:text-[84px] lg:px-10 lg:mt-[100px]
+    md:text-[48px] md:px-[125px]
+    text-[24px] px-5 mt-[28px]">{text}</h2>
     <a href={link} class="font-Manrope font-semibold
     lg:font-inter lg:font-normal lg:font-semibold lg:text-base lg:leading-6 lg:mt-[48px] lg:mb-[108px]
-    md:font-inter md:font-normal md:font-semibold md:text-base md:leading-6 md:mt-[48px] md:mb-[47px]
-    text-sm leading-4 font-sans mt-[16px] mb-[29px] px-6 py-3 bg-black text-white dark:bg-white dark:text-black rounded-full shadow-lg">{button}</a>
+    md:font-inter md:font-normal md:font-semibold md:text-base md:leading-6 md:mt-[24px] md:mb-[47px]
+    text-[12px] leading-4 font-sans mt-[16px] mb-[29px] px-6 py-3 bg-black text-white dark:bg-white dark:text-black rounded-full shadow-lg">{button}</a>
     </div>
     </div>
     </div>

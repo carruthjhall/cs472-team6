11fb3092acaa7746794b1e0b2a0c7a25b5f87ae9
WuliuQ <doubixian@gmail.com>
2023-04-19 19:43:23 -0700

    Update files

--- src/components/BlogPage/BlogPage.jsx
+++ src/components/BlogPage/BlogPage.jsx
@@ -3,8 +3,8 @@ export default function BlogPage({options}) {
     const {header, paragraph, image,} = options;
     const valueArray = paragraph.split(String.fromCharCode(10));
     return (
-        <div className={`bg-white text-black dark:bg-black dark:text-white py-6 lg:px-40 md:px-20 px-10`}>
-        <div className= "font-normal font-baijamjuree">
+        <div className={`flex bg-white text-black dark:bg-black dark:text-white py-6 lg:px-40 md:px-20 lg:max-w-[1036px] md:max-w-[864px] max-w-[340px] mx-auto`}>
+        <div className= "font-normal font-main">
             <h2 className='mb-4 font-bold text-3xl leading-10'>{header}</h2> 
             {image && (<img src={image} class="w-full h-auto aspect-w-1 aspect-h-1" alt="blogpicture"/>)}
             <div className="font-inter font-normal font-main
--- src/components/CtaSection/CtaSection.jsx
+++ src/components/CtaSection/CtaSection.jsx
@@ -1,20 +1,28 @@
 import vectorSVG from "./vector.svg";
+import vectorSVG2 from "./vector2.svg";
 export default function CtaSection({options}) {
   const {link, word1, word2, gradient} = options;
-  
+  const isgreenblue = gradient === 'from-green-400 to-blue-500';
+
   return (
-    <div className={`bg-white text-black dark:bg-black dark:text-white`}>
-    <div className={'py-20 px-10 md:px-20 lg:px-40'}>
-    <div className="font-bold font-baijamjuree
-    lg:text-6xl lg:px-20
+    <div className={`flex bg-white text-black dark:bg-black dark:text-white lg:max-w-[1036px] md:max-w-[864px] max-w-[340px] mx-auto item-center`}>
+    <div className={'py-20 px-auto md:px-20 lg:px-20'}>
+    <div className="font-bold font-main
+    lg:text-6xl lg:px-10
     md:text-5xl 
     sm:font-semibold sm:text-lg">
-        <h2>{word1}</h2> 
-        <div className = {`bg-white dark:bg-black`}>
-        <div className = {`bg-gradient-to-r ${gradient} text-transparent bg-clip-text flex items-center`}>
-        {link.trim() ? <a href={link}className= 'flex items-center font-bold font-main flex-wrap mt-2 mb-2 lg:text-6xl lg:leading-non md:text-5xl md:leading-tight sm:font-medium sm:text-2xl sm:leading-relaxed'>{word2}<img src={vectorSVG} className="w-6 h-6 md:w-14 md:h-14 lg:w-18 lg:h-18"/></a> : null}
-        </div>
-      </div>
+    <h2>{word1}</h2> 
+    <div className = {`bg-white dark:bg-black`}>
+    <div className = {`bg-gradient-to-r ${gradient} text-transparent bg-clip-text flex items-center`}>
+    <a href={link.trim() ? link : '#0'}className= 'flex items-center font-bold font-main flex-wrap mt-2 mb-2 lg:text-6xl lg:leading-non md:text-5xl md:leading-tight sm:font-medium sm:text-2xl sm:leading-relaxed'>{word2}
+    {isgreenblue ? (
+        <img src={vectorSVG2} className="w-6 h-6 md:w-14 md:h-14 lg:w-18 lg:h-18"/>
+      ) : (
+        <img src={vectorSVG} className="w-6 h-6 md:w-14 md:h-14 lg:w-18 lg:h-18"/>
+    )}
+    </a>
+    </div>
+    </div>
     </div>
     </div>
     </div>

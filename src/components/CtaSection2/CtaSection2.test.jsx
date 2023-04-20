7e3dcc7b1e442a23f0266b15024fc526e7139789
WuliuQ <doubixian@gmail.com>
2023-04-20 11:14:07 -0700

    add coverage test

---
+++ src/components/CtaSection2/CtaSection2.test.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { describe, expect, it } from 'vitest';
+import { Gradients } from '../../utils/utils';
+import CtaSection2 from './CtaSection2';
+import { registeredComponents } from '../../utils/registeredComponents';
+
+
+describe('CtaSection2 Component', () => {
+    it('Text renders properly', () => {
+        let options = {
+            link: "https://www.google.com/", 
+            text: "Interested in working with me?", 
+            button: "GET IN TOUCH",
+            gradient: Gradients.default
+        }
+
+        // render the component
+        const component = renderer.create(
+            <CtaSection2 options={options}/>
+        )
+
+        // get component version of dom
+        let displayedWords = component.toJSON();
+        // get text;
+        displayedWords = displayedWords.children[0].children[0].children[0].children;
+
+        // expect the rendered/displayed words to match the options passed into the component
+        expect(displayedWords[0]).eq(options.text);
+    })
+
+    it('CtaSection2Options show correct input values',()=>{
+        // get the CtaSection2 registered component
+        let CtaSection2Component = registeredComponents.get('CtaSection2');
+        let OptionsComponent = CtaSection2Component.optionsComponent;
+        let defaultOptions = CtaSection2Component.defaultOptions;
+
+        // render the CtaSection2Options component
+        const component = renderer.create(
+            <OptionsComponent options={defaultOptions} updateComponent={() => null}  />
+        )
+
+
+        let optionsComponentJSON = component.toJSON();
+
+        // get the input components from the json
+        let linkInput = optionsComponentJSON.children[1];
+        let textInput = optionsComponentJSON.children[3];
+        let buttonInput = optionsComponentJSON.children[5];
+        let gradientInput = optionsComponentJSON.children[7];
+
+        // make sure that the value of the link input was set properly
+        expect(linkInput.props.value).eq(defaultOptions.link);
+        // make sure that the value of the text input was set properly
+        expect(textInput.props.value).eq(defaultOptions.text);
+        // make sure that the value of the button input was set properly
+        expect(buttonInput.props.value).eq(defaultOptions.button);
+        // make sure that the value of the gradient input was set properly
+        expect(gradientInput.props.value).eq(defaultOptions.gradient);
+    })
+
+    it('CtaSectionOptions handleChange function',()=>{
+        // get the CtaSection2 registered component
+        let CtaSection2Component = registeredComponents.get('CtaSection2');
+        let OptionsComponent = CtaSection2Component.optionsComponent;
+        let defaultOptions = CtaSection2Component.defaultOptions;
+
+        // render the CtaSection2Options component
+        // In updateComponent prop make sure that component handleChange function is working as expected
+        const component = renderer.create(
+            <OptionsComponent options={defaultOptions} updateComponent={(change) => expect(JSON.stringify(change)).eq(JSON.stringify({gradient: Gradients["green-blue"]}))}  />
+        )
+
+        // Get handle change function from component
+        let handleChange = component.toTree().rendered.rendered[1].props.onChange;
+
+        // simulate a input change, specifically the gradient changing
+        handleChange({target: {
+            name: 'gradient',
+            value: Gradients["green-blue"]
+        }})
+    })
+
+})

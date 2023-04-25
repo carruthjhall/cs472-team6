b39bf5ba68280e5e492f317b9ba5baba5cae7673
WuliuQ <doubixian@gmail.com>
2023-04-25 14:30:54 -0700

    Update BlogPage.jsx

--- src/components/BlogPage/BlogPage.jsx
+++ src/components/BlogPage/BlogPage.jsx
@@ -4,7 +4,7 @@ export default function BlogPage({options}) {
     const valueArray = paragraph.split(String.fromCharCode(10));
     return (
         <div className={`flex bg-white text-black dark:bg-black dark:text-white py-6 md:px-[80px] lg:max-w-[1036px] md:max-w-[864px] max-w-[340px] w-full mx-auto item-center`}>
-        <div className= "font-normal">
+        <div className= "font-normal w-full">
             <h2 className='mb-4 font-main text-3xl leading-10'>{header}</h2> 
             {image && (<img src={image} class="w-full aspect-w-1 aspect-h-1" alt="blogpicture"/>)}
             <div className="font-inter font-normal

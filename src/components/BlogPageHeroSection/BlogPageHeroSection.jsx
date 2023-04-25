60868313a65998bd99e234baa7c3ed2bd7522ed7
Kristy Nguyen <kln95624@gmail.com>
2023-04-25 16:57:44 -0700

    Fixed minor padding issues and made tailwind aspects cleaner

--- src/components/BlogPageHeroSection/BlogPageHeroSection.jsx
+++ src/components/BlogPageHeroSection/BlogPageHeroSection.jsx
@@ -3,19 +3,19 @@ export default function BlogPageHeroSection({options}) {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <div className="text-center" style={{ paddingTop: "238px" }}>
+      <div className="text-center pt-[238px]">
         {/* Title */}
-        <p className="text-4xl sm:text-5xl md:text-6xl lg:text-5xl xl:text-4xl 2xl:text-5xl font-bold font-main text-black dark:text-white mb-4 mx-auto" style={{maxWidth: "1080px"}}>
+        <p className="text-4xl sm:text-5xl md:text-6xl lg:text-5xl xl:text-4xl 2xl:text-5xl font-bold font-main text-black dark:text-white mb-4 mx-auto max-w-[1080px]">
         {title}
         </p>
         
         {/* Name */}
-        <p className = {`text-lg sm:text-xl md:text-2xl lg:text-lg xl:text-base 2xl:text-lg font-bold font-main break-words bg-gradient-to-r ${gradient} inline-block text-transparent mb-12 bg-clip-text`} style={{ maxWidth: "289px" }}>
+        <p className = {`text-lg sm:text-xl md:text-2xl lg:text-lg xl:text-base 2xl:text-lg font-bold font-main break-words bg-gradient-to-r ${gradient} inline-block text-transparent mb-12 bg-clip-text max-w-[289px]`}>
         {name}
         </p>
 
         {/* Image */}
-        <img src={image} className="w-full max-w-full mb-12" />
+        <img src={image} className="w-full max-w-full mb-12 pb-40" />
 
       </div>
     </div>

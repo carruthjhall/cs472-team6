079055f8241eaa2625762f368d8ecaa12889459d
WuliuQ <doubixian@gmail.com>
2023-04-25 14:27:32 -0700

    Update BlogPage.jsx

--- src/components/BlogPage/BlogPage.jsx
+++ src/components/BlogPage/BlogPage.jsx
@@ -3,11 +3,11 @@ export default function BlogPage({options}) {
     const {header, paragraph, image,} = options;
     const valueArray = paragraph.split(String.fromCharCode(10));
     return (
-        <div className={`flex bg-white text-black dark:bg-black dark:text-white py-6 md:px-[80px] lg:max-w-[1036px] md:max-w-[864px] max-w-[340px] mx-auto item-center`}>
-        <div className= "font-normal font-main">
-            <h2 className='mb-4 font-bold text-3xl leading-10'>{header}</h2> 
-            {image && (<img src={image} class="w-full h-auto aspect-w-1 aspect-h-1" alt="blogpicture"/>)}
-            <div className="font-inter font-normal font-main
+        <div className={`flex bg-white text-black dark:bg-black dark:text-white py-6 md:px-[80px] lg:max-w-[1036px] md:max-w-[864px] max-w-[340px] w-full mx-auto item-center`}>
+        <div className= "font-normal">
+            <h2 className='mb-4 font-main text-3xl leading-10'>{header}</h2> 
+            {image && (<img src={image} class="w-full aspect-w-1 aspect-h-1" alt="blogpicture"/>)}
+            <div className="font-inter font-normal
             sm:text-sm sm:leading-relaxed 
             md:text-lg md:leading-6
             lg:text-lg lg:leading-4">{valueArray.map((value, index) => (

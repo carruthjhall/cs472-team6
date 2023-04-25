7250ffec1737c9869129b55fc10dc20045dd42bf
WuliuQ <doubixian@gmail.com>
2023-04-25 08:04:54 -0700

    change class to className in CtaSection2.jsx

--- src/components/CtaSection2/CtaSection2.jsx
+++ src/components/CtaSection2/CtaSection2.jsx
@@ -10,12 +10,12 @@ export default function CtaSection2({options}) {
     rounded-bl-[32px] rounded-br-[32px]
     w-auto h-auto
     flex justify-center items-center`}>
-    <div class="flex flex-col items-center text-center">
-    <h2 class="font-bold font-main 
+    <div className="flex flex-col items-center text-center">
+    <h2 className="font-bold font-main 
     lg:text-[84px] lg:px-15 lg:mt-[80px]
     md:text-[48px] md:px-[125px]
     text-[24px] px-10 mt-[28px]">{text}</h2>
-    <a href={link} class="font-Manrope font-semibold
+    <a href={link} className="font-Manrope font-semibold
     lg:font-inter lg:font-normal lg:font-semibold lg:text-base lg:leading-6 lg:mt-[48px] lg:mb-[108px]
     md:font-inter md:font-normal md:font-semibold md:text-base md:leading-6 md:mt-[24px] md:mb-[47px]
     text-[12px] leading-4 font-sans mt-[16px] mb-[29px] px-6 py-3 bg-black text-white dark:bg-white dark:text-black rounded-full shadow-lg">{button}</a>

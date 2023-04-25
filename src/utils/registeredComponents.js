16c72d6746d9c1257190e49a0c0be77b5daf3cd8
Yunhuiww <122959510+Thindatuo@users.noreply.github.com>
2023-04-25 15:08:47 -0700

    Update registeredComponents.js
    add a missing comma that caused the tests to fail

--- src/utils/registeredComponents.js
+++ src/utils/registeredComponents.js
@@ -59,6 +59,6 @@ export const registeredComponents = new Map([
     ['Navbar', NavBarComponent],
     ['Footer', FooterComponent],
     ['Companies', CompaniesComponent],
-    ['MoreArticles', MoreArticlesComponent]
+    ['MoreArticles', MoreArticlesComponent],
     ['Services Section', ServicesSectionComponent]
 ]);

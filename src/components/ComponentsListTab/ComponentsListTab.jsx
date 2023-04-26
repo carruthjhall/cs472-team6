b6bd39c19d034f559ee5e93b6a72c77025e61b7a
Zach C <zacheryec001@gmail.com>
2023-04-26 15:43:27 -0700

    Fixed double add bug

--- src/components/ComponentsListTab/ComponentsListTab.jsx
+++ src/components/ComponentsListTab/ComponentsListTab.jsx
@@ -16,9 +16,9 @@ export default function ComponentsListTab(){
 function ComponentPreview({component}) {
     let addComponent = PageState((state) => state.addComponent);
     return (
-        <div className="shadow-md w-[300px] mt-4">
+        <div className="shadow-md w-[300px] mt-4" onClick={() => addComponent(new PageComponent(component.name, component.defaultOptions))}>
             <img src={component.componentPreview} className="cursor-pointer" alt="Component Preview" />
-            <div className="flex justify-between p-3" onClick={() => addComponent(new PageComponent(component.name, component.defaultOptions))}>
+            <div className="flex justify-between p-3">
                 <p>{component.name}</p>
                 <button className="cursor-pointer">Add</button>
             </div>

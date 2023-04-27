ba5838115882631344be8b4c5ad01a3ccd01c35b
alexfox361 <71406323+alexfox361@users.noreply.github.com>
2023-04-27 12:10:53 -0700

    Add Tests and Light/Dark Toggle
    Added tests for the component and light/dark toggles.

--- src/components/_Projects/Projects.jsx
+++ src/components/_Projects/Projects.jsx
@@ -6,7 +6,7 @@ export default function Projects({options}) {
         <div className="mx-auto w-[340px] md:w-[740px] lg:w-[1200px]">
             <div className="mb-[32px] md:w-[560px] lg:w-[1200px] lg:flex lg:flex-row lg:justify-between">
                 <div className="text-[32px] md:text-[56px] lg:text-[64px]">
-                    <p className="text-white font-main">
+                    <p className="dark:text-white font-main">
                         {Title}
                     </p>
                     <p className={`break-words bg-gradient-to-r ${gradient} inline-block text-transparent bg-clip-text font-main`}>
@@ -14,7 +14,7 @@ export default function Projects({options}) {
                     </p>
                 </div>
                 <div className="h-[32px]"></div>
-                <div className="text-white font-secondary text-[16px] md:text-[18px] lg:w-[560px] lg:my-auto">
+                <div className="dark:text-white font-secondary text-[16px] md:text-[18px] lg:w-[560px] lg:my-auto">
                     {Description}
                 </div>
             </div>
@@ -28,10 +28,10 @@ export default function Projects({options}) {
 function ProjectItem({ProjectItems}) {
     const { Title, Description, Picture, URL } = ProjectItems;
     return (
-        <div className="w-[340px] h-[370px] lg:h-[587px] lg:w-[560px] bg-white rounded-xl mx-auto my-[40px]">
+        <div className="w-[340px] h-[370px] lg:h-[587px] lg:w-[560px] dark:bg-gray-700 bg-gray-400 rounded-xl mx-auto my-[40px]">
             <img className="mx-auto h-[242px] lg:h-[400px]" src={Picture}></img>
-            <div className="h-[128px] lg:h-[187px] bg-gray-900">
-                <div className="text-white font-main p-[29px] lg:p-[42px]">
+            <div className="h-[128px] lg:h-[187px] dark:bg-gray-900 bg-gray-100">
+                <div className="dark:text-white font-main p-[19px] lg:p-[42px]">
                     <div className="text-[26px] lg:text-[32px]">
                         {Title}
                     </div>
--- src/components/_Projects/ProjectsComponent.js
+++ src/components/_Projects/ProjectsComponent.js
@@ -24,10 +24,10 @@ export const ProjectsComponent = new Component(
         gradient: Gradients.default,
         Description: "Lorem ipsum dolor sit amet consectetur. Consequat tempus rhoncus cum in. Vel id donec adipiscing elit morbi amet. Fermentum aliquam sed vitae placerat quam nec convallis.", 
         Projects: [
-            new ProjectOptions('Finance Landing Page', 'Landing Page UI Kit', "https://images.unsplash.com/photo-1576153192396-180ecef2a715?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80", ''),
-            new ProjectOptions('Interior Design Website', 'Dribble Shot', "https://images.unsplash.com/photo-1576153192396-180ecef2a715?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80", ''),
-            new ProjectOptions('Russell Morgan Portfolio', 'Dribble Shot', "https://images.unsplash.com/photo-1576153192396-180ecef2a715?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80", ''),
-            new ProjectOptions('Sonali Landing Page', 'Dribble Shot', "https://images.unsplash.com/photo-1576153192396-180ecef2a715?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80", '')
+            new ProjectOptions('Finance Landing Page', 'Landing Page UI Kit', "https://github.com/cs472-team6/cs472-team6-resources/blob/main/images/ProjectOne.png?raw=true", ''),
+            new ProjectOptions('Interior Design Website', 'Dribble Shot', "https://github.com/cs472-team6/cs472-team6-resources/blob/main/images/ProjectTwo.png?raw=true", ''),
+            new ProjectOptions('Russell Morgan Portfolio', 'Dribble Shot', "https://github.com/cs472-team6/cs472-team6-resources/blob/main/images/ProjectThree.png?raw=true", ''),
+            new ProjectOptions('Sonali Landing Page', 'Dribble Shot', "https://github.com/cs472-team6/cs472-team6-resources/blob/main/images/ProjectFour.png?raw=true", '')
         ]
     }
 );

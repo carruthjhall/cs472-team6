6f79f56a3252ce704760daf4d6a6dcc94ca4f836
Zach C <zacheryec001@gmail.com>
2023-04-27 00:33:29 -0700

    Fixed character bug in header

--- src/components/TestimonialSection/TestimonialSection.jsx
+++ src/components/TestimonialSection/TestimonialSection.jsx
@@ -7,7 +7,7 @@ export default function TestimonialSection({options}){
     const {gradient, noOfTestimonials, testimonialList} = options;
     return(
         <div className="lg:w-[1028px] m-3 lg:m-auto flex flex-col">
-            <h2 className="text-main font-bold text-[32px] md:text-[64px] mt-11 mb-11 dark:text-[#F0F2F5] flex justify-center">Client &#20;<span className={`text-transparent bg-clip-text bg-gradient-to-r ${gradient}`}> Testimonials</span></h2>
+            <h2 className="text-main font-bold text-[32px] md:text-[64px] mt-11 mb-11 dark:text-[#F0F2F5] flex justify-center">Client&nbsp;<span className={`text-transparent bg-clip-text bg-gradient-to-r ${gradient}`}> Testimonials</span></h2>
             <div className="flex flex-wrap justify-center flex_col">
                 {testimonialList.slice(0,noOfTestimonials).map((testimonial, index) => {
                     return(

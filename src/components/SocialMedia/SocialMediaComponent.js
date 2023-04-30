a6be7f57c676b702dd26ac40e5158c1c492e9d20
CruzG-Dylan <dylan.cruzgogue@gmail.com>
2023-04-30 11:33:50 -0700

    updated SocialMediaComponent.js

--- src/components/SocialMedia/SocialMediaComponent.js
+++ src/components/SocialMedia/SocialMediaComponent.js
@@ -4,6 +4,13 @@ import SocialMedia from "./SocialMedia";
 import SocialMediaOptions from "./SocialMediaOptions";
 import SocialMediaPreview from "./SocialMediaPreview.png";
 
+export class SocialOptions {
+    constructor(imagelink, link){
+        this.imagelink = imagelink;
+        this.link = link;
+    }
+}
+
 export const SocialMediaComponent = new Component(
     'Social Media', 
     SocialMedia, 
@@ -11,10 +18,17 @@ export const SocialMediaComponent = new Component(
     SocialMediaPreview, 
     {
         title: "Follow Me", body: "Im Katrik Bansal, a designer who works with startups to build brands.", 
-        image: "https://github.com/cs472-team6/cs472-team6-resources/blob/main/images/SocialMedia_image.png?raw=true", gradient: Gradients.default,
-        FbLink: "https://www.facebook.com", LiLink: "https://www.linkedin.com", 
-        TwLink: "https://www.twitter.com", InLink: "https://www.instagram.com",
-        DrLink: "https://www.dribbble.com", BhLink: "https://www.behance.net"
+        imageUrl: "https://github.com/cs472-team6/cs472-team6-resources/blob/main/images/SocialMedia_image.png?raw=true", gradient: Gradients.default,
+        socials: [
+            new SocialOptions("https://github.com/cs472-team6/cs472-team6-resources/blob/main/images/facebook.png?raw=true", ""),
+            new SocialOptions("https://github.com/cs472-team6/cs472-team6-resources/blob/main/images/linkedIn.png?raw=true", ""),
+            new SocialOptions("https://github.com/cs472-team6/cs472-team6-resources/blob/main/images/twitter.png?raw=true", ""),
+            new SocialOptions("https://github.com/cs472-team6/cs472-team6-resources/blob/main/images/instagram.png?raw=true", ""),
+            new SocialOptions("https://github.com/cs472-team6/cs472-team6-resources/blob/main/images/dribble.png?raw=true", ""),
+            new SocialOptions("https://github.com/cs472-team6/cs472-team6-resources/blob/main/images/behance.png?raw=true", "")
+
+        ],
+        socialCount: 6
     }
 
 )

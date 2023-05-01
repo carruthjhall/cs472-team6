fe3bd8dab9a2eb92cb5af61fe5c7d7c4844e2af6
Kristy Nguyen <kln95624@gmail.com>
2023-04-30 18:13:06 -0700

    Removed the h-screen element that may have caused the component overlap

--- src/components/BlogPageHeroSection/BlogPageHeroSection.jsx
+++ src/components/BlogPageHeroSection/BlogPageHeroSection.jsx
@@ -2,7 +2,7 @@ export default function BlogPageHeroSection({options}) {
   const {title, name, gradient, image} = options;
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center">
       <div className="text-center pt-[238px]">
         {/* Title */}
         <p className="text-4xl sm:text-5xl md:text-6xl lg:text-5xl xl:text-4xl 2xl:text-5xl font-bold font-main text-black dark:text-white mb-4 mx-auto max-w-[1080px]">
